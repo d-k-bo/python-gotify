316d006cdae98d04a85092e735001186eca74fdd
d-k-bo <47948262+d-k-bo@users.noreply.github.com>
2022-06-07 18:36:50 +0200

    Use bump2version

--- gotify/__init__.py
+++ gotify/__init__.py
@@ -2,9 +2,6 @@
 
 from __future__ import annotations
 
-__version__ = "0.5"
-
-
 from .async_gotify import AsyncGotify
 from .errors import GotifyConfigurationError, GotifyError
 from .gotify import Gotify
